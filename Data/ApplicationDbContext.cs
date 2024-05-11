e4011f764614ca59d8cd8bc86ce6653a819a34cc
MudassirAdmani <mudassiradmani466@gmail.com>
2024-05-12 02:19:22 +0500

    updates

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ImgCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImgCrud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
