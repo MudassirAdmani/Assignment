39f0c255f240a0ee892bb9a8a94517bd89e061a2
MudassirAdmani <mudassiradmani466@gmail.com>
2024-05-12 01:32:26 +0500

    refresh

--- Models/AddProductViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NetCoreAssignment.Models
-{
-    public class AddProductViewModel
-    {
-        public string Name { get; set; }
-        public int Cost { get; set; }
-    }
-}
--- Models/Entities/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NetCoreAssignment.Models.Entities
-{
-    public class Product
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public int Cost { get; set; }
-    }
-}
